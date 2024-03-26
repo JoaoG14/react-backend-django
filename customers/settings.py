df2f608275b37d83a1810f0c236170fe7e4bc436
João <joaog.alves14@gmail.com>
2024-03-26 10:52:02 -0300

    refresh access token updated

--- customers/settings.py
+++ customers/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKE_LIFETIME': timedelta(minutes=15),
+    'ROTATE_REFRESH_TOKENS': True,
+}
 
 # Application definition
 
